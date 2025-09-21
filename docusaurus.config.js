92abe09cc7d13c7d17ead843b94fd00c8e5a70e9
Fabrice <fab.hatege15@gmail.com>
2025-09-21 02:23:22 +0200

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/fabriceHategekimana',
+  url: 'https://fabricehategekimana.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/site_siloe',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
